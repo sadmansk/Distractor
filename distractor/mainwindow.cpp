8acc5885025f8094970b2be60e89f469fe30e9f8
Sadman Kazi <sadmansazidk@gmail.com>
2015-03-21 17:55:53 -0400

    Added the timer wireframe

--- distractor/mainwindow.cpp
+++ distractor/mainwindow.cpp
@@ -8,9 +8,22 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     setWindowTitle(tr("Distractor"));
+    minTime = 5 * 60000;
+    maxTime = 20 * 60000; //time in minutes
+
+    interval = rand() % (maxTime - minTime) + minTime;
+    timer.start(interval, this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::timerEvent (QTimerEvent *event) {
+    //gets called at a random interval between 5 and 20 mins
+
+    //reset the timer
+    interval = rand() % (maxTime - minTime) + minTime;
+    timer.start(interval, this);
 }
--- distractor/mainwindow.h
+++ distractor/mainwindow.h
@@ -15,8 +15,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void timerEvent (QTimerEvent *event) Q_DECL_OVERRIDE;
+
 private:
     Ui::MainWindow *ui;
+    QBasicTimer timer;
+    //set up timer parameters
+    int minTime, maxTime;
+    int interval;
 };
 
 #endif // MAINWINDOW_H
